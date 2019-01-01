94b9d09e92a25a38463ca123aa6359c22abb8628
DESKTOP-2FISH38\seong <b5460881@naver.com>
2019-01-01 21:46:33 +0900

    .

---
+++ 컴프2/Hw09/ObjectStreamTest.java
@@ -0,0 +1,29 @@
+package Hw09;
+import java.util.*;
+import java.io.*;
+public class ObjectStreamTest {
+
+	public static void main(String[] args) throws IOException{
+		ObjectInputStream in=null;
+		ObjectOutputStream out=null;
+		try {
+			int c;
+			
+			out=new ObjectOutputStream(new FileOutputStream("object.dat"));
+			out.writeObject(new Date());
+			
+			out.flush();
+			in=new ObjectInputStream(new FileInputStream("object.dat"));
+			Date d=(Date) in.readObject();
+			System.out.println(d);
+			
+		}catch (ClassNotFoundException e) {
+		}finally {
+			if(in!=null)
+				in.close();
+			if(out!=null)
+				out.close();
+		}
+	}
+
+}
